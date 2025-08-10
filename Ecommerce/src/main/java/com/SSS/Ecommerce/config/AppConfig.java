90c32ee7700f4a37f88c1187508ff5e88f6e8d29
Vraj <virajhiwale05@gmail.com>
2025-08-10 20:00:55 +0530

    Add backend functionality

---
+++ Ecommerce/src/main/java/com/SSS/Ecommerce/config/AppConfig.java
@@ -0,0 +1,68 @@
+package com.SSS.Ecommerce.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.PasswordManagementDsl;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/**").authenticated()
+                        .anyRequest().permitAll()
+                )
+                .addFilterBefore(new JwtValidator(), BasicAuthenticationFilter.class) // Add your filter here if any
+                .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.configurationSource(new CorsConfigurationSource() {
+
+
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                        CorsConfiguration cfg = new CorsConfiguration();
+                        cfg.setAllowedOrigins(Arrays.asList(
+                                "http://localhost:3000",
+                                "http://localhost:4200"
+                        ));
+                        cfg.setAllowedMethods(Collections.singletonList("*"));
+                        cfg.setAllowCredentials(true);
+                        cfg.setAllowedHeaders(Collections.singletonList("*"));
+                        cfg.setExposedHeaders(Arrays.asList("Authorization"));
+                        cfg.setMaxAge(3600L);
+                        return cfg;
+                    }
+                }));
+
+        //FOR LOGIN FORM TO SEND USERNAME AND PASSWORD
+        http.httpBasic(withDefaults());
+        http.formLogin(withDefaults());
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ Ecommerce/src/main/java/com/SSS/Ecommerce/config/JwtConstant.java
@@ -0,0 +1,7 @@
+package com.SSS.Ecommerce.config;
+
+public class JwtConstant {
+
+    public static final String SECRET_KEY="qwertyuioplkjhgfdsazxcvbnm";
+    public static final String JWT_HEADER="Authorization";
+}
